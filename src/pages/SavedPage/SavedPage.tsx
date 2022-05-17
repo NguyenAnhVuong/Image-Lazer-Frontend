9e8d772a5d50d7895cbecac2ce00813e9879c6f2
quanganhquanganh <59202082+quanganhquanganh@users.noreply.github.com>
2022-05-17 13:10:48 +0700

    [Feature/UserPage] Test.

--- src/pages/SavedPage/SavedPage.tsx
+++ src/pages/SavedPage/SavedPage.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 
 const SavedPage = () => {
-  const [saved, setSaved] = useState<any[]>([]);
+  const [saved] = useState<any[]>([]);
   return (
     <div>
       <h1>Saved</h1>
       <ul>
-        {saved.map(item => (
+        {saved.map((item) => (
           <li key={item.id}>{item.title}</li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default SavedPage;
