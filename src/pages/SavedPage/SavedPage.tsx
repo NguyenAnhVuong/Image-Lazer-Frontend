ba3082a5a10d5cf6ab3bf0f005f9da5fd3fe0a63
quanganhquanganh <59202082+quanganhquanganh@users.noreply.github.com>
2022-05-17 13:03:22 +0700

    [Feature] Add SavedPage.

---
+++ src/pages/SavedPage/SavedPage.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const SavedPage = () => {
+  const [saved, setSaved] = useState<any[]>([]);
+  return (
+    <div>
+      <h1>Saved</h1>
+      <ul>
+        {saved.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SavedPage;
