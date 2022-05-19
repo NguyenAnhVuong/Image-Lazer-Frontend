081c86faf822a088389e0898454c04f70fd82a2d
NguyenAnhVuong <navuong2001@gmail.com>
2022-05-19 21:57:28 +0700

    [Feature] loginsuccess to display header

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { HistoryRouter as Router } from 'redux-first-history/rr6';
 import App from './App';
-import { history, store } from './app/store';
-import Header from './components/Layout/Header';
+import { store } from './app/store';
 import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router history={history}>
-        <Header />
-        <App />
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>,
 );
